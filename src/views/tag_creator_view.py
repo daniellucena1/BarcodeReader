38dcfe412e1794fc3aec10d049764e9755254f68
Daniel Torres <danieljlt23@gmail.com>
2024-02-08 05:43:49 -0300

    feat: finalization of the implementation of business rules

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,5 +1,6 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
+from src.controllers.tag_creator_controller import CreateTagController
 
 class TagCreatorView:
     '''
@@ -7,10 +8,13 @@ class TagCreatorView:
     '''
 
     def validade_and_create(self, http_request: HttpRequest) -> HttpResponse:
+        tag_creator_controller = CreateTagController()
+
         body = http_request.body
         product_code = body["product_code"]
 
-        # logic of deals rules
-        print('Estou na minha view')
+        # logic of business rules
+        formatted_response = tag_creator_controller.create(product_code)
+
         # http return
-        return HttpResponse(status_code=200, body={"hello": "world"})
+        return HttpResponse(status_code=200, body=formatted_response)
