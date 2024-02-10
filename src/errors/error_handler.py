bd10198847d73fc03b37f3e78195fbcde55fe22a
Daniel Torres <danieljlt23@gmail.com>
2024-02-09 22:19:43 -0300

    feat: Implementig validator

--- src/errors/error_handler.py
+++ src/errors/error_handler.py
@@ -1,6 +1,18 @@
 from src.views.http_types.http_response import HttpResponse
+from src.errors.errors_type.http_unprocessable_entity import HttpeUnprocessableEntityError
 
 def handler_errors(error: Exception) -> HttpResponse: 
+    if isinstance(error, HttpeUnprocessableEntityError):
+        return HttpResponse(
+            status_code=error.status_code,
+            body={
+                "errors": [{
+                    "title": error.name,
+                    "detail": error.message
+                }]
+            }
+        )
+
     return HttpResponse(
         status_code=500,
         body={
